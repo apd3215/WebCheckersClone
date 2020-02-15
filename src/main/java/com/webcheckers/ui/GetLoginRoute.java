9a0a7720c279777258da0bdfbd2c1180255a6a5f
jmb3471 <47160881+jmb3471@users.noreply.github.com>
2020-02-15 15:53:29 -0500

    made login() static

--- src/main/java/com/webcheckers/ui/GetLoginRoute.java
+++ src/main/java/com/webcheckers/ui/GetLoginRoute.java
@@ -1,19 +1,16 @@
 package com.webcheckers.ui;
 
-import spark.Route;
-import spark.TemplateEngine;
+import spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import spark.Request;
-import spark.ModelAndView;
-import spark.Response;
 
 public class GetLoginRoute implements Route {
     
     private final TemplateEngine templateEngine;
+
 
     public GetLoginRoute(final TemplateEngine templateEngine) {
         this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required!");
@@ -22,6 +19,7 @@ public class GetLoginRoute implements Route {
     @Override
     public Object handle(Request request, Response response) {
         Map<String, Object> vm = new HashMap<>();
+
         return templateEngine.render(new ModelAndView(vm, "signin.ftl"));
     }
 }
