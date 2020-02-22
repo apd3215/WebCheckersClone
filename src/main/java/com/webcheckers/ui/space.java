9caa4e37d4b1847db4579d1bac4c3b03813e7e96
Andre Da Costa <apd3215@rit.edu>
2020-02-22 16:34:06 -0500

    Space class w/ constructor, needs methods

--- src/main/java/com/webcheckers/ui/space.java
+++ src/main/java/com/webcheckers/ui/space.java
@@ -4,4 +4,12 @@ public class space {
     private int cellIdx;
     private boolean isValid;
     private Piece piece;
+
+    Public Space space(int cellIdx, boolean isValid, Piece piece){
+        this.cellIdx = cellIdx;
+        this.isValid = isValid;
+        this.piece   = piece;
+    }
+
+
 }
