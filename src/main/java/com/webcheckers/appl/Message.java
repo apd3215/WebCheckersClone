779f8d79c7ea3d8b7873f48414b5517816ab9dca
Joshua Yoder <git@jpfyoder.com>
2020-02-29 14:49:30 -0500

    fixed errors

--- src/main/java/com/webcheckers/appl/Message.java
+++ src/main/java/com/webcheckers/appl/Message.java
@@ -1,14 +1,11 @@
-package com.webcheckers.appl
+package com.webcheckers.appl;
 
-
-
-class Message {
-
-    public enum Type = { INFO, ERROR }
+public class Message {
+    public enum Type { INFO, ERROR }
     public String test;
     public Type type;
 
-    Message(String test, Tyte type) {
+    public Message(String test, Type type) {
         this.test = test;
         this.type = type;
     }
