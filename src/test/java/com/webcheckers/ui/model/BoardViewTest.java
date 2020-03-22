f4fd40656d4ff8facafa88e5d19b1a2e11be9c50
Joe Netti <nettijoe@gmail.com>
2020-03-22 19:36:25 -0400

    BoardView test coverage

---
+++ src/test/java/com/webcheckers/ui/model/BoardViewTest.java
@@ -0,0 +1,95 @@
+package com.webcheckers.ui.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.webcheckers.model.Piece;
+import com.webcheckers.model.Row;
+import com.webcheckers.model.Space;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.webcheckers.model.BoardView;
+
+import java.util.Iterator;
+
+
+public class BoardViewTest {
+
+    @Test
+    public void light_spaces_empty() {
+        final BoardView CuT = new BoardView();
+        CuT.setBoard();
+        Iterator<Row> rowIterator= CuT.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Iterator<Space> spaceIterator = row.iterator();
+            while (spaceIterator.hasNext()) {
+                Space space = spaceIterator.next();
+                Piece piece = space.getPiece();
+                Space.Color color = space.getColor();
+                if (color == Space.Color.LIGHT) {
+                    assertNull(piece);
+                }
+            }
+        }
+    }
+
+
+    @Test
+    public void white_pieces_init() {
+        final BoardView CuT = new BoardView();
+        CuT.setBoard();
+        Iterator<Row> rowIterator= CuT.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            int rowIdx = row.getIndex();
+            Iterator<Space> spaceIterator = row.iterator();
+            while (spaceIterator.hasNext()) {
+                Space space = spaceIterator.next();
+                Piece piece = space.getPiece();
+                Space.Color color = space.getColor();
+                if (rowIdx <= 2) {
+                    if (color == Space.Color.DARK) {
+                        assertEquals(piece.type, Piece.PieceType.SINGLE);
+                        assertEquals(piece.color, Piece.PieceColor.WHITE);
+                    }
+                }
+                else {
+                    if (piece != null) {
+                        assertNotEquals(piece.color, Piece.PieceColor.WHITE);
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Test
+    public void red_pieces_init() {
+        final BoardView CuT = new BoardView();
+        CuT.setBoard();
+        Iterator<Row> rowIterator= CuT.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            int rowIdx = row.getIndex();
+            Iterator<Space> spaceIterator = row.iterator();
+            while (spaceIterator.hasNext()) {
+                Space space = spaceIterator.next();
+                Piece piece = space.getPiece();
+                Space.Color color = space.getColor();
+                if (rowIdx >= 5) {
+                    if (color == Space.Color.DARK) {
+                        assertEquals(piece.type, Piece.PieceType.SINGLE);
+                        assertEquals(piece.color, Piece.PieceColor.RED);
+                    }
+                }
+                else {
+                    if (piece != null) {
+                        assertNotEquals(piece.color, Piece.PieceColor.RED);
+                    }
+                }
+            }
+        }
+    }
+
+}
