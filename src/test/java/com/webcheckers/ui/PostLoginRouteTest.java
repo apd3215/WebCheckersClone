9d61b950876ff1f44b95ebafe3a0b7f18007f79a
drs4554 <drs4554@rit.edu>
2020-03-30 11:39:12 -0400

    postloginroute test written

--- src/test/java/com/webcheckers/ui/PostLoginRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostLoginRouteTest.java
@@ -1,31 +1,27 @@
 package com.webcheckers.ui;
 
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
-import com.webcheckers.util.Message;
+import com.webcheckers.appl.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import spark.TemplateEngine;
-import spark.HaltException;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Session;
 
 import com.webcheckers.Application;
-import com.webcheckers.appl.Player;
 import com.webcheckers.appl.PlayerLobby;
-import com.webcheckers.model.Game;
-import com.webcheckers.ui.GetGameRoute;
-import com.webcheckers.ui.TemplateEngineTester;
+
+import java.util.Hashtable;
 
 /**
- * GetGameRouteTest is a testing suite for the GetGameRoute class.
- * @author Joshua Yoder
+ * PostLoginRouteTest is a testing suite for the PostLoginRoute class.
+ * @author Dhaval Shrishrimal
  */
 public class PostLoginRouteTest {
     /**
@@ -41,7 +37,7 @@ public class PostLoginRouteTest {
     private Response response;
     private TemplateEngine engine;
     private PlayerLobby playerLobby;
-   // private final TemplateEngineTester testhelper = new TemplateEngineTester();
+    private Player player;
 
     /**
      * Test Attributes
@@ -49,11 +45,6 @@ public class PostLoginRouteTest {
     private String USERW = "user";
     private String PASSW = "pass";
     private String empty = "";
-    private static final Message NAME_ERR = Message.error("Username cannot be empty AND cannot contain special characters.");
-    private static final Message WRONG = Message.error("Wrong Password OR Username already exists.");
-    private static final Message PASS = Message.error("Password Cannot be empty.");
-    private static final Message ALREADY = Message.error("Player already signed in.");
-    private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
 
     private static final String TITLE_ATTR = "title";
     private static final String TITLE = "Sign In";
@@ -72,20 +63,25 @@ public class PostLoginRouteTest {
         response = mock(Response.class);
         engine = mock(TemplateEngine.class);
         playerLobby = mock(PlayerLobby.class);
-        playerLobby.getUsers().put(USERW, PASSW);
+        playerLobby.sign_in(USERW, PASSW);
+        player = mock(Player.class);
+
+
         //Put mock playerLobby in our application
         Application.playerLobby = playerLobby;
-
 
         // create a unique CuT for each test
         CuT = new PostLoginRoute(engine);
     }
 
     /**
-     * Test for return values.
+     * Test for name error.
      */
     @Test
     public void login_game_nameERR(){
+
+        when(request.queryParams(eq(USERNAME))).thenReturn(empty);
+        when(request.queryParams(eq(PASSWORD))).thenReturn(empty);
 
         // Template engine tester
         final TemplateEngineTester testHelper = new TemplateEngineTester();
@@ -98,11 +94,135 @@ public class PostLoginRouteTest {
         testHelper.assertViewModelExists();
         testHelper.assertViewModelIsaMap();
 
+        // assert view model data
         testHelper.assertViewModelAttribute(TITLE_ATTR, TITLE);
+        testHelper.assertViewModelAttribute(MSG, PostLoginRoute.NAME_ERR);
 
+        //assert view
+        testHelper.assertViewName("signin.ftl");
 
+    }
 
+    /**
+     * Test for user already signed in.
+     */
+    @Test
+    public void login_game_signed(){
 
+        when(request.queryParams(eq(USERNAME))).thenReturn(USERW);
+        when(request.queryParams(eq(PASSWORD))).thenReturn(PASSW);
+        when(playerLobby.sign_in(USERW, PASSW)).thenReturn(-2);
+
+        // Template engine tester
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        //call handle on the CuT
+        CuT.handle(request, response);
+
+        //Test viewmodel
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+        // assert view model data
+        testHelper.assertViewModelAttribute(TITLE_ATTR, TITLE);
+        testHelper.assertViewModelAttribute(MSG, PostLoginRoute.ALREADY);
+
+        //assert view
+        testHelper.assertViewName("signin.ftl");
+
+    }
+
+    /**
+     * Test for checking if the password can be empty.
+     */
+    @Test
+    public void login_game_pass(){
+
+        when(request.queryParams(eq(USERNAME))).thenReturn(USERW);
+        when(request.queryParams(eq(PASSWORD))).thenReturn(empty);
+        when(playerLobby.sign_in(USERW, empty)).thenReturn(-1);
+
+        // Template engine tester
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        //call handle on the CuT
+        CuT.handle(request, response);
+        System.out.println(Application.playerLobby.sign_in(USERW, empty));
+
+        //Test viewmodel
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+        // assert view model data
+        testHelper.assertViewModelAttribute(TITLE_ATTR, TITLE);
+        testHelper.assertViewModelAttribute(MSG, PostLoginRoute.PASS);
+        //assert view
+        testHelper.assertViewName("signin.ftl");
+
+    }
+
+    /**
+     * Test for checking if the password is wrong.
+     */
+    @Test
+    public void login_game_pass_wrong(){
+
+        when(request.queryParams(eq(USERNAME))).thenReturn(USERW);
+        when(request.queryParams(eq(PASSWORD))).thenReturn(PASSWORD);
+        when(playerLobby.sign_in(USERW, PASSWORD)).thenReturn(3);
+
+        // Template engine tester
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        //call handle on the CuT
+        CuT.handle(request, response);
+
+        //Test viewmodel
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+        // assert view model data
+        testHelper.assertViewModelAttribute(TITLE_ATTR, TITLE);
+        testHelper.assertViewModelAttribute(MSG, PostLoginRoute.WRONG);
+
+        //assert view
+        testHelper.assertViewName("signin.ftl");
+
+    }
+
+    /**
+     * Test for checking if the password is wrong.
+     */
+    @Test
+    public void login_game(){
+
+        when(request.queryParams(eq(USERNAME))).thenReturn(USERW);
+        when(request.queryParams(eq(PASSWORD))).thenReturn(PASSW);
+        when(playerLobby.sign_in(USERW, PASSW)).thenReturn(1);
+        Hashtable<String, Player> Players = new Hashtable<>();
+        Players.put(USERW, player);
+        when(playerLobby.getPlayers()).thenReturn(Players);
+
+        // Template engine tester
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        //call handle on the CuT
+        CuT.handle(request, response);
+
+        //Test viewmodel
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+        // assert view model data
+        testHelper.assertViewModelAttribute(TITLE_ATTR, TITLE);
+        testHelper.assertViewModelAttribute(MSG, PostLoginRoute.WELCOME_MSG);
+
+        //assert view
+        testHelper.assertViewName("home.ftl");
 
     }
 
