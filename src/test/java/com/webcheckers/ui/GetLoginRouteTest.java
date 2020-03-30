b9eb951cb7650c66d27cc9d67bd4a3390a6d2519
Andre Da Costa <apd3215@rit.edu>
2020-03-30 17:51:53 -0400

    created GetLoginRoute unit test

---
+++ src/test/java/com/webcheckers/ui/GetLoginRouteTest.java
@@ -0,0 +1,68 @@
+package com.webcheckers.ui;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import spark.TemplateEngine;
+import spark.HaltException;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import com.webcheckers.Application;
+import com.webcheckers.appl.Player;
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Game;
+import com.webcheckers.ui.GetGameRoute;
+import com.webcheckers.ui.TemplateEngineTester;
+
+/**
+ * GetGameRouteTest is a testing suite for the GetGameRoute class.
+ * @author Andre Da Costa
+ */
+public class GetLoginRouteTest {
+    private GetLoginRoute CuT;
+
+    private Request request;
+    private Session session;
+    private Response response;
+    private TemplateEngine engine;
+
+    /**
+     * Setup new mock objects for each test.
+     */
+    @BeforeEach
+    public void setup() {
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+        engine = mock(TemplateEngine.class);
+
+
+        // create a unique CuT for each test
+        CuT = new GetLoginRoute(engine);
+    }
+
+    public void testLogIn(){
+        //Template engine tester
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        //Call handle on the CuT
+        CuT.handle(request, response);
+
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+        testHelper.assertViewModelAttribute("title", "Sign In");
+
+
+    }
+}
